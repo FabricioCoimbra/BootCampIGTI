b8eb4b1f58ff87be99e54a3e28e98787c05cf570
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-09-19 23:20:20 -0300

    Adicionado filtro de título aos posts (Consome muita memória se houverem muitos objetos)

--- Java/MavenQuickStart/testMavenVSCode/src/main/java/com/Fabricio/App.java
+++ Java/MavenQuickStart/testMavenVSCode/src/main/java/com/Fabricio/App.java
@@ -9,6 +9,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 import java.io.*;
@@ -17,13 +18,43 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 
-@Getter
-@Setter
-class PostUser{
+class PostUser {
     private Integer userId;
     private Integer id;
     private String title;
     private String body;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 }
 
 @Getter
@@ -42,12 +73,23 @@ public class App {
         System.out.println("Carregando Lista de filmes do herokuapp");
         System.out.println();
         System.out.println(get("https://igti-film.herokuapp.com/api/films", autenticar()));
+
         System.out.println();        
-        System.out.println("Carregando Posts do arquivo"); 
-          
+        System.out.println("Carregando Posts do arquivo");           
         System.out.println();       
-        final ArrayList<PostUser> posts = carregarPostsDoArquivo();
+        ArrayList<PostUser> posts = carregarPostsDoArquivo();
         System.out.println(gson.toJson(posts));
+
+        System.out.println();        
+        System.out.println("Filtrando Posts com titulo dolorem");           
+        System.out.println();  
+        posts = filtrarTitle("dolorem", posts);
+        for(PostUser post: posts){
+            System.out.println(gson.toJson(post));
+            System.out.println();  
+        }
+        
+        
     }
 
     private static String autenticar() {
@@ -103,5 +145,12 @@ public class App {
             System.out.println(e); 
             return new ArrayList();
         }        
+    }
+
+    private static ArrayList<PostUser> filtrarTitle(String filtro, ArrayList<PostUser> posts){
+        // essa função aparentemente inocente duplica 2x o array, e faz um for para filtrar, além é clarro da dupla conversão de tipos...
+        return (ArrayList<PostUser>) posts.stream()
+            .filter(post -> post.getTitle().contains(filtro))
+            .collect(Collectors.toList());
     }
 }
